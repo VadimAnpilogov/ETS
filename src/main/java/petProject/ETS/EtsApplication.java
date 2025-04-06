fe2fe1a30bd8021777f660b5c4dfff7ec984204d
VadimAnpilogov <vadick.anpilogov2015@yandex.ru>
2025-04-06 18:17:58 +0700

    Основа приложения для запуска

---
+++ src/main/java/petProject/ETS/EtsApplication.java
@@ -0,0 +1,13 @@
+package petProject.ETS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/petProject/ETS/EtsApplicationTests.java
@@ -0,0 +1,13 @@
+package petProject.ETS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
